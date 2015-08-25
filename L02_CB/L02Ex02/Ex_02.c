fcfb828b19b08d5386cd9ed02dded111a1a989c9
xxxxx <xxxx.@gmail.com>
2015-08-25 09:58:28 -0300

    L02Ex02 in building.

---
+++ L02_CB/L02Ex02/Ex_02.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+	int N1, N2; //Duas entradas para comparar se são multiplos
+	scanf("%d", &N1);
+	scanf("%d", &N2);
+	int maior = N1 > N2 ? N1 : N2;
+	int menor = N1 < N2 ? N1 : N2;
+	if((maior/menor) == )
+	{
+		
+	}
+
+
+
+
+	
+
+
+
+
+	return 0;
+}
