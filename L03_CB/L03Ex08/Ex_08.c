5606068680c88252546265eff14cd9f4a2d45792
Danrley Willyan da Silva Pereira <danrley@localhost.localdomain>
2015-09-20 15:22:58 -0300

    Done to L03Ex20.

--- L03_CB/L03Ex08/Ex_08.c
+++ L03_CB/L03Ex08/Ex_08.c
@@ -1,18 +1,55 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
-void get_digitInt_in_positionN(int digitInt, int positionN)
-{
-	
-	
-	
+
+unsigned int total_digits(int number) {
+	if (number < 0) {
+		number *= -1;
+	}
+
+	return ((int) (log(number)/log(10))) + 1;
 }
 
 
-int main()
-{
-	get_digitInt_in_positionN();
-	
-	
-	
+unsigned short get_digit(int number, unsigned int position) {
+	unsigned int mod_position = pow(10, position);
+
+	return number%mod_position / (mod_position/10);
+}
+
+
+int reverse(int number) {
+	unsigned short is_negative = 0;
+
+	if (number < 0) {
+		number *= -1;
+		is_negative = 1;
+	}
+
+	unsigned short digits = total_digits(number);
+	int result = 0;
+	int i;
+
+	for (i = 1; digits > 0; ++i, --digits) {
+		unsigned short single_digit = get_digit(number, i);
+
+		result += (single_digit*((int) pow(10, digits))) / 10;
+	}
+
+	if (is_negative) {
+		result *= -1;
+	}
+
+	return result;
+}
+
+
+int main() {
+	int number;
+	scanf("%d", &number);
+	printf("%d\n", reverse(number));
+
+
+	return 0;
 }
--- L03_CB/L03Ex19/Ex_19.c
+++ L03_CB/L03Ex19/Ex_19.c
@@ -1,14 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(){
-	int N;
-	scanf("%d", &N);
-	if( N%2 == 1)
+int maior_menor(int *matriz_adress)
+{
+	int i, maior=0, menor = 32000;
+        
+	for(i=0; i<20; i++)
 	{
-	printf("IMPAR\n");
+		int N;
+		scanf("%d", &N);
+		maior = maior>N ? maior : N;
+		menor = menor<N ? menor : N;
 	}
-	else printf("PAR\n");
+        *matriz_adress = menor;
+        matriz_adress++;
+        *matriz_adress = maior;
+	return maior, menor;
+}
+
+int main(){
+	int i = 0;
+	int maior_Menor[2] = {};
+        maior_menor(&maior_Menor[0]);
+
+	printf("%d\n", maior_Menor[i++]);
+	printf("%d\n", maior_Menor[i]);
 
 	return 0;
 }
