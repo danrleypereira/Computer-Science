98491ece813321c8b8944a49acf8c008e9a2798c
Danrley Willyan <danrleywillyan@outlook.com>
2020-06-04 10:30:34 -0300

    List03UDF Done with some JUnit tests

--- CB/Java/lista03UDF/api/ExercisesAPI.java
+++ CB/Java/lista03UDF/api/ExercisesAPI.java
@@ -2,7 +2,6 @@ package api;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-
 import javax.swing.JOptionPane;
 
 public class ExercisesAPI {
@@ -13,12 +12,50 @@ public class ExercisesAPI {
 		return "MAIOR: " + numbers[2] + "\nMENOR: " + numbers[0];
 	}
 	
-	public static String Exercise06( ArrayList<Integer> numbers ) {
+	public static int Exercise06( ArrayList<Integer> numbers ) {
 		int sum = 0;
 		for (int i = 0; i < (numbers.size()); i++) {
 			sum += numbers.get(i).intValue();
 		}
 		
-		return "MÉDIA ARITMÉTICA: " + sum/numbers.size() ;
+		return sum/numbers.size();
+	}
+	
+	public static String Exercise08() {
+		String stringReturn = "";
+		for(int i=1; i<200; i++) {
+			if (i%9 == 0) stringReturn += "\n"; 
+			stringReturn += (i++ + ", ");
+		}
+		return stringReturn;
+	}
+	
+	public static String Exercise04(float[] vetor1, float[] vetor2) {
+		String stringReturn = "";
+		int lenghtVector = vetor1.length > vetor2.length ? vetor1.length : vetor2.length; 
+		Float[] vetor3 = new Float[lenghtVector];
+		for(int i=0; i<lenghtVector; i++) {
+			try {
+				vetor3[i] = vetor1[i]+vetor2[i];
+				stringReturn += ( vetor1[i] + " + " + vetor2[i] + " = " + vetor3[i] + "\n");
+			} catch (java.lang.ArrayIndexOutOfBoundsException e) {
+				JOptionPane.showMessageDialog(null, "Vetores tÃªm tamanhos diferentes;", "", JOptionPane.ERROR_MESSAGE);
+			}
+		}
+		return stringReturn;
+	}
+	
+	public static String Exercise01(Float[] vetor1) {
+		String stringReturn = "";
+		Float[] vetor2 = new Float[vetor1.length];
+		for(int i=0; i<vetor1.length; i++) {
+			try {
+				vetor2[i] = vetor1[i]*2;
+				stringReturn += ( vetor1[i] + " * 2 = " + vetor2[i] + "\n");
+			} catch (java.lang.ArrayIndexOutOfBoundsException e) {
+				JOptionPane.showMessageDialog(null, "Vetores tÃªm tamanhos diferentes;", "", JOptionPane.ERROR_MESSAGE);
+			}
+		}
+		return stringReturn;
 	}
 }
--- CB/Java/lista03UDF/view/ExercisesView.java
+++ CB/Java/lista03UDF/view/ExercisesView.java
@@ -1,7 +1,5 @@
 package view;
 
-import api.ExercisesAPI;
-
 import java.util.ArrayList;
 
 import java.awt.FlowLayout;
@@ -17,16 +15,31 @@ public class ExercisesView extends JFrame implements ActionListener {
 	private JButton[] exercisesButtons = new JButton[10];  //exercise label
 	
 	public ExercisesView() {
-		super( "ProgramaÁ„o de computadores - AvaliaÁ„o 03" );
+		super( "Programa√ß√£o de computadores - Avalia√ß√£o 03" );
 		setLayout( new FlowLayout() );
 		
-		exercisesButtons[5] = new JButton("ExercÌcio 06");
-		exercisesButtons[5].setToolTipText("Este È o ExercÌcio 06");
+		exercisesButtons[0] = new JButton("Exerc√≠cio 01");
+		exercisesButtons[0].setToolTipText("Este √© o Exerc√≠cio 01");
+		exercisesButtons[0].addActionListener(this);
+		add(exercisesButtons[0]);
+		
+		exercisesButtons[3] = new JButton("Exerc√≠cio 04");
+		exercisesButtons[3].setToolTipText("Este √© o Exerc√≠cio 04");
+		exercisesButtons[3].addActionListener(this);
+		add(exercisesButtons[3]);
+		
+		exercisesButtons[5] = new JButton("Exerc√≠cio 06");
+		exercisesButtons[5].setToolTipText("Este √© o Exerc√≠cio 06");
 		exercisesButtons[5].addActionListener(this);
 		add(exercisesButtons[5]);
 		
-		exercisesButtons[9] = new JButton("ExercÌcio 10");
-		exercisesButtons[9].setToolTipText("Este È o ExercÌcio 10");
+		exercisesButtons[7] = new JButton("Exerc√≠cio 08");
+		exercisesButtons[7].setToolTipText("Este √© o Exerc√≠cio 08");
+		exercisesButtons[7].addActionListener(this);
+		add(exercisesButtons[7]);
+		
+		exercisesButtons[9] = new JButton("Exerc√≠cio 10");
+		exercisesButtons[9].setToolTipText("Este √© o Exerc√≠cio 10");
 		exercisesButtons[9].addActionListener(this);
 		add(exercisesButtons[9]);
 		
@@ -37,44 +50,105 @@ public class ExercisesView extends JFrame implements ActionListener {
 	public void actionPerformed( ActionEvent ae ) {
 		String action = ae.getActionCommand();
 		switch(action) {
-			case "ExercÌcio 10":
+			case "Exerc√≠cio 10":
 				Integer[] numbers = new Integer[3];
-				JOptionPane.showMessageDialog(this, "Informe 3 n˙meros inteiros!");
+				JOptionPane.showMessageDialog(this, "Informe 3 n√∫meros inteiros!");
 				try {
 					numbers[0] = Integer.parseInt( 
-										JOptionPane.showInputDialog(this, "Informe o primeiro n˙mero:")
+										JOptionPane.showInputDialog(this, "Informe o primeiro n√∫mero:")
 								);
 					numbers[1] = Integer.parseInt( 
-										JOptionPane.showInputDialog(this, "Informe o segundo n˙mero:")
+										JOptionPane.showInputDialog(this, "Informe o segundo n√∫mero:")
 								);
 					numbers[2] = Integer.parseInt( 
-										JOptionPane.showInputDialog(this, "Informe o terceiro n˙mero:")
+										JOptionPane.showInputDialog(this, "Informe o terceiro n√∫mero:")
 								); 
 					JOptionPane.showMessageDialog(this, api.ExercisesAPI.Exercise10(numbers));
 				} catch (java.lang.NumberFormatException e) {
-					JOptionPane.showMessageDialog(this, "N„o foi informado um n˙mero inteiro!", "Errou", 
+					JOptionPane.showMessageDialog(this, "N√£o foi informado um n√∫mero inteiro!", "Errou", 
 							JOptionPane.ERROR_MESSAGE);
 				};
-			case "ExercÌcio 06":
+				break;
+			case "Exerc√≠cio 06":
 				ArrayList<Integer> values = new ArrayList<Integer>();
-				JOptionPane.showMessageDialog(this, "Valores para c·lculo de mÈdia aritmÈtica, valor negativo sari e calcula a mÈdia.");
+				JOptionPane.showMessageDialog(this, "Valores para c√°lculo de m√©dia aritm√©tica, valor negativo sai e calcula a m√©dia.");
 				try {
 					do {
 						values.add( 
 								Integer.parseInt( 
-											JOptionPane.showInputDialog(this, "Informe o valor n˙mero "+
+											JOptionPane.showInputDialog(this, "Informe o valor n√∫mero "+
 												values.size()
 												+":"
 											)
 								)
 						);
 					} while(values.get( values.size()-1 ).intValue() > 0);
-					values.remove(values.size()-1); //remove o n˙mero negativo
-					JOptionPane.showMessageDialog(this, api.ExercisesAPI.Exercise06(values));
+					values.remove(values.size()-1); //remove o n√∫mero negativo
+					JOptionPane.showMessageDialog(this, "M√âDIA ARITM√âTICA: "+ api.ExercisesAPI.Exercise06(values));
 				} catch (java.lang.NumberFormatException e) {
-					JOptionPane.showMessageDialog(this, "N„o foi informado um n˙mero inteiro!", "Errou", 
+					JOptionPane.showMessageDialog(this, "N√£o foi informado um n√∫mero inteiro!", "Errou", 
 							JOptionPane.ERROR_MESSAGE);
 				}
+				break;
+			case "Exerc√≠cio 08":
+				JOptionPane.showMessageDialog(this, "N√∫mero impares entre 100 e 200:\n" + api.ExercisesAPI.Exercise08());
+				break;
+			case "Exerc√≠cio 04":
+				float[] vetor1 = {2, 99, 44, 23, 44, 976, 23};
+				float[] vetor2 = {21, 299, 344, 823, 34, 76};
+				JOptionPane.showMessageDialog(this, "Soma de dois vetores:\n" + api.ExercisesAPI.Exercise04(vetor1, vetor2));
+				break;
+			case "Exerc√≠cio 01":
+				Float[] vetor = new Float[5];
+				JOptionPane.showMessageDialog(this, "Informe 5 n√∫meros!");
+				try {
+					vetor[0] = Float.parseFloat( 
+										JOptionPane.showInputDialog(this, "Informe o primeiro n√∫mero:")
+								);
+					vetor[1] = Float.parseFloat( 
+										JOptionPane.showInputDialog(this, "Informe o segundo n√∫mero:")
+								);
+					vetor[2] = Float.parseFloat( 
+										JOptionPane.showInputDialog(this, "Informe o terceiro n√∫mero:")
+								); 
+					vetor[3] = Float.parseFloat( 
+							JOptionPane.showInputDialog(this, "Informe o quarto n√∫mero:")
+					);
+					vetor[4] = Float.parseFloat( 
+							JOptionPane.showInputDialog(this, "Informe o quinto n√∫mero:")
+					);
+					JOptionPane.showMessageDialog(this, api.ExercisesAPI.Exercise01(vetor));
+				} catch (java.lang.NumberFormatException e) {
+					JOptionPane.showMessageDialog(this, "N√£o foi informado um n√∫mero inteiro!", "Errou", 
+							JOptionPane.ERROR_MESSAGE);
+				};
+				break;
+				
 		}
 	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
