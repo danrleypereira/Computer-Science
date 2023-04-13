664f05763d3c07fbcdb3e4dbca8966358a1e54dd
danrleypereira <danrleywillian@gmail.com>
2023-04-13 15:54:08 -0300

    First deliverable project

--- praticas/testes/conjuntos/README.md
+++ praticas/testes/conjuntos/README.md
@@ -1,1 +1,11 @@
+## creating exe file
 g++ -std=c++11 textFile.cpp -o exeFile.out
+
+## creating exe file to GDB
+g++ -std=c++11 textFile.cpp -o -g exeFile.out
+
+### gdb commands
+- b line_to_breakpoint
+- p sentence_to_print
+- https://www.bitdegree.org/learn/gdb-debugger
+- https://www.geeksforgeeks.org/gdb-step-by-step-introduction/
--- praticas/testes/conjuntos/main.cpp
+++ praticas/testes/conjuntos/main.cpp
@@ -5,33 +5,55 @@
 struct set
 {
     size_t length;
-    int* elements;
+    int *elements;
 };
 
-//references
+// references
 void intersection(set A, set B);
 void difference(set A, set B);
 void unionF(set A, set B);
-int* getElementsFromQueue(std::queue<int> g);
-int howManyElements(set G);
-int howManyElements(set G);
-int nthElement(int n, int* address);
+int *getElementsFromQueue(std::queue<int> g);
+int nthElement(int n, int *address);
 void showSet(set G);
-//end of references
+// end of references
 
-int main(int argc, char** argv) {
-    //colete os dados dos conjuntos A e B
+set get_set()
+{
+    setlocale(LC_ALL, "Portuguese");
+    int qtd;
+    int* elements = (int *)malloc(sizeof(int) * qtd);
+    printf("Tell me how many integers numbers this SET will have:");
+    scanf("%i", &qtd);
+    for (int i = 0; i < qtd; i = i + 1)
+    {
+        printf("Tell me the %iº number:", (i+1));
+        scanf("%i", &elements[i]);
+        bool isRepeated = false;
+        do {
+            isRepeated = false;
+            for(int j=0; j < i; j++)
+                if( nthElement(j, elements) == nthElement(i, elements)) {
+                    isRepeated = true;
+                    printf("This number already exists on SET. \nTell me the %iº number again:", (i+1));
+                    scanf("%i", &elements[i]);
+                }
+        } while(isRepeated);
+    }
+    set aux = {.length = qtd, .elements = elements};
+    return aux;
+}
+
+int main(int argc, char **argv)
+{
+    // colete os dados dos conjuntos A e B
     std::cout << "Just numbers beloging to Integer set.\n";
-    int Aelements[10] = {444, 678, 904, 1000, 888, 432, 555, 3209, 19089, 55};
-    int* Aadress = (int*) malloc(sizeof(int) * 10);
-    Aadress = Aelements;
-    set A = {.length = 10, .elements = Aadress};
-    int Belements[8] = {444, 678, 904, 77, 76, 432, 7};
-    int* Badress = (int*) malloc(sizeof(int) * 8);
-    Badress = Belements;
-    set B = {.length = 8, .elements = Badress};
-    std::cout << "criado os dois conjuntos\n";
-    // std::cout << (int) *A.elements + sizeof(int)*2 << "\n";
+    set A = get_set();
+    set B = get_set();
+    std::cout << "Now you have two sets called set A and set B\n";
+    std::cout << "A ";
+    showSet(A);
+    std::cout << "B ";
+    showSet(B);
     unionF(A, B);
     intersection(A, B);
     difference(A, B);
@@ -39,89 +61,105 @@ int main(int argc, char** argv) {
     return 0;
 }
 // functions
-void intersection(set A, set B) 
+void intersection(set A, set B)
 {
     std::queue<int> intersectionQueue;
-    //who's greater?
-    // int greaterSet = ALength < BLength ? BLength: ALength;
-    for(int aIndex=0; aIndex<A.length; aIndex++){
-        for(int bIndex=0; bIndex<B.length; bIndex++){
-            if(A.elements[aIndex] == B.elements[bIndex])
+    // who's greater?
+    //  int greaterSet = ALength < BLength ? BLength: ALength;
+    for (int aIndex = 0; aIndex < A.length; aIndex++)
+    {
+        for (int bIndex = 0; bIndex < B.length; bIndex++)
+        {
+            if (A.elements[aIndex] == B.elements[bIndex])
                 intersectionQueue.push(B.elements[bIndex]);
         }
     }
-    //return a new SET with the intersecction elements
+    // return a new SET with the intersecction elements
     set intersection = {
-        .length = intersectionQueue.size(), 
-        .elements = getElementsFromQueue(intersectionQueue)
-    };
+        .length = intersectionQueue.size(),
+        .elements = getElementsFromQueue(intersectionQueue)};
+    std::cout << "\nThe intersection set is : \n";
+    std::cout << "A ^ B ";
     showSet(intersection);
 }
 
-void unionF(set A, set B) 
+void unionF(set A, set B)
 {
     std::set<int> unionSet;
-    for(int aIndex=0; aIndex<A.length; aIndex++){
+    for (int aIndex = 0; aIndex < A.length; aIndex++)
+    {
         unionSet.insert(A.elements[aIndex]);
     }
-    for(int bIndex=0; bIndex<B.length; bIndex++){
+    for (int bIndex = 0; bIndex < B.length; bIndex++)
+    {
         unionSet.insert(B.elements[bIndex]);
     }
 
     // printing union
-    std::set<int, std::greater<int> >::iterator itr;
+    std::set<int, std::greater<int>>::iterator itr;
     std::cout << "\nThe union set is : \n";
-    for (itr = unionSet.begin(); itr != unionSet.end(); itr++) {
-        std::cout << *itr << " ";
+    std::cout << "A U B = { ";
+    for (itr = unionSet.begin(); itr != unionSet.end(); itr++)
+    {
+        std::cout << *itr << ", ";
     }
+    std::cout << " };";
     std::cout << std::endl;
 }
 
-void difference(set A, set B) 
+void difference(set A, set B)
 {
     std::set<int> differenceSet;
-    for(int aIndex=0; aIndex<A.length; aIndex++){
+    for (int aIndex = 0; aIndex < A.length; aIndex++)
+    {
         bool existsInB = false;
-        for(int bIndex=0; bIndex<B.length; bIndex++){
-            if(A.elements[aIndex] == B.elements[bIndex])
+        for (int bIndex = 0; bIndex < B.length; bIndex++)
+        {
+            if (A.elements[aIndex] == B.elements[bIndex])
                 existsInB = true;
         }
-        //↓↓ add elements of A if it does not exist in B ↓↓
-        if(!existsInB) differenceSet.insert(A.elements[aIndex]);
+        // ↓↓ add elements of A if it does not exist in B ↓↓
+        if (!existsInB)
+            differenceSet.insert(A.elements[aIndex]);
     }
     // printing union
-    std::set<int, std::greater<int> >::iterator itr;
+    std::set<int, std::greater<int>>::iterator itr;
     std::cout << "\nThe difference set is : \n";
-    for (itr = differenceSet.begin(); itr != differenceSet.end(); itr++) {
-        std::cout << *itr << " ";
+    std::cout << "A - B = { ";
+    for (itr = differenceSet.begin(); itr != differenceSet.end(); itr++)
+    {
+        std::cout << *itr << ", ";
     }
+    std::cout << " };";
     std::cout << std::endl;
 }
 
-//utils
-void showSet(set G) {
-    std::cout << "<<Conjunto>>\n";
-    std::cout << "Tamanho: " << G.length << "\n";
-    std::cout << "<<Elementos do conjunto>>\n";
-    int length = howManyElements(G);
-    for(int i =0; i < length; i++){
-        std::cout << "Elemento nº" << i+1 << " = " << G.elements[i] << "\n";
+// utils
+void showSet(set G)
+{
+
+    std::cout << " = { ";
+    for (int i = 0; i < G.length; i++)
+    {
+        std::cout << G.elements[i] << (G.length == (i+1) ? "": ", ");
     }
+    std::cout << " };\n";
 }
-int* getElementsFromQueue(std::queue<int> g) {
+int *getElementsFromQueue(std::queue<int> g)
+{
     // int array[g.size()] = {};
-    int* elementsFromQueue = (int*) malloc(sizeof(int) * g.size());
-    int i=0;
-    while (!g.empty()) {
-        *(elementsFromQueue + i) = g.front(); 
+    int *elementsFromQueue = (int *)malloc(sizeof(int) * g.size());
+    int i = 0;
+    while (!g.empty())
+    {
+        *(elementsFromQueue + i) = g.front();
         g.pop();
         i++;
     }
     return elementsFromQueue;
 }
-int howManyElements(set G){
-    return sizeof(G.elements) / 2; //2 = int size
-}
-int nthElement(int n, int* address){
+
+int nthElement(int n, int *address)
+{
     return *(address + n);
 }
